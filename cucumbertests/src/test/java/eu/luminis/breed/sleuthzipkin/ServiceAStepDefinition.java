8a914fa301d7d229be3ca29f6163bed46868de73
rafaela <rafaela.breed@luminis.eu>
2018-04-09 21:16:54 +0200

    Minor fixes

--- cucumbertests/src/test/java/eu/luminis/breed/sleuthzipkin/ServiceAStepDefinition.java
+++ cucumbertests/src/test/java/eu/luminis/breed/sleuthzipkin/ServiceAStepDefinition.java
@@ -12,6 +12,7 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import eu.luminis.breed.sleuthzipkin.model.AModel;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
