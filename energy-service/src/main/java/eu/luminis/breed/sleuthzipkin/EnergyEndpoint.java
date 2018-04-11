c38d7ec2574dc81024f1434766734e19d0293d20
rafaela <rafaela.breed@luminis.eu>
2018-04-11 21:23:41 +0200

    Just some more logical log & numbers...

--- energy-service/src/main/java/eu/luminis/breed/sleuthzipkin/EnergyEndpoint.java
+++ energy-service/src/main/java/eu/luminis/breed/sleuthzipkin/EnergyEndpoint.java
@@ -16,17 +16,17 @@ public class EnergyEndpoint {
   @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getEnergyRequest")
   @ResponsePayload
   public GetEnergyResponse getEnergyResponse(@RequestPayload GetEnergyRequest request) {
-    logger.info("Get country");
+    logger.info("Get energy");
     GetEnergyResponse response = new GetEnergyResponse();
     Energy energy = new Energy();
     if (request.getName() == Device.TOASTER) {
-      energy.setValue(1500);
+      energy.setValue(5);
       energy.setUnit(Unit.K_WH);
     } else if (request.getName() == Device.STOVE) {
       energy.setValue(1);
       energy.setUnit(Unit.M_3);
     } else if (request.getName() == Device.COFFEEMACHINE) {
-      energy.setValue(1000);
+      energy.setValue(75);
       energy.setUnit(Unit.K_WH);
     }
     response.setEnergy(energy);
