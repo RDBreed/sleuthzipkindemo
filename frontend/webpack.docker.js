7930fedffe18c47194330f32a1e9811ea0cb7abc
rafaela <rafaela.breed@luminis.eu>
2019-03-29 17:13:24 +0100

    add specific docker npm run config

---
+++ frontend/webpack.docker.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: ['./src/js/load.js'],
+    output: {
+        publicPath: '/',
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        chunkFilename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: true,
+            addDevelopmentFeatures:true,
+        })
+    ],
+    devServer: {
+        contentBase: [
+            path.join(__dirname, "dist"),
+            path.join(__dirname, 'src')
+        ],
+        compress: true,
+        host: '0.0.0.0',
+        port: 8080
+    }
+};
